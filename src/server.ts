4eb750870eeace09209e4420014932e82fa929b6
Leonardo César <leo_cesa@hotmail.com>
2023-09-28 15:32:55 -0300

    back to serverless

--- src/server.ts
+++ src/server.ts
@@ -26,20 +26,20 @@ app.register(uploadVideoDiskRoute);
 app.register(createVideoTranscriptionRoute);
 app.register(generateTextRoute);
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3333;
+// const port = process.env.PORT ? Number(process.env.PORT) : 3333;
 
-app
-  .listen({
-    port: port,
-    host: "0.0.0.0",
-  })
-  .then(() => {
-    console.log(`HTTP server running on port ${port}`);
-  });
+// app
+//   .listen({
+//     port: port,
+//     host: "0.0.0.0",
+//   })
+//   .then(() => {
+//     console.log(`HTTP server running on port ${port}`);
+//   });
 
-export default app;
+// export default app;
 
-// export default async (req: any, res: any) => {
-//   await app.ready();
-//   app.server.emit('request', req, res);
-// }
+export default async (req: any, res: any) => {
+  await app.ready();
+  app.server.emit('request', req, res);
+}
